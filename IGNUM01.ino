88ccd81e27c3aa323a8fe635d5dbbd41d9b08fd5
TitanDoctorMachine <labdrago@gmail.com>
2022-06-15 21:24:30 +0000

    non functional version

--- IGNUM01.ino
+++ IGNUM01.ino
@@ -1,12 +1,11 @@
-//hello world!
 #include <SHA256.h>
 #include <Crypto.h>
 #include <RNG.h>
 
-SHA256 sha256;
+SHA256 sha256, userhash;
 
-byte UserSessionid[32];
-byte Challenge[32];
+byte UserSessionid[32], Challenge[32];
+String KeyChallenge;
 
 void setup() {
 
@@ -28,21 +27,29 @@ void newchallenge() {
   sha256.reset();
   sha256.update(Challenge, 32);
   sha256.finalize(Challenge, 32); //dá a saída hash na própria var -- 32 by == 256 bits
-  String KeyChallenge;
   for (int i = 0; i != 32; i++) {
     KeyChallenge += String (Challenge[i], HEX);
     }
-    int KeyChallengeint = KeyChallenge.toInt();
     Serial.println("ChallengeKey:");
     Serial.println(KeyChallenge);
 }
 
 void validateuser(){
+  
   byte* UserAuth = Challenge;
-  UserAuth += '00c95efe198f454eadb6811831eac0d93b8c12476bd88b558abfb68b1251d2ba'; // DOCMAC012021111123654825362425623
-  sha256.reset();
-  sha256.update(UserAuth, 64);
-  sha256.finalize(UserSessionid, 32);
+  //UserAuth += '00c95efe198f454eadb6811831eac0d93b8c12476bd88b558abfb68b1251d2ba'; // DOCMAC012021111123654825362425623
+ 
+  //Debug
+  String UserAuthStr;
+  for (int i = 0; i != 32; i++) {
+    UserAuthStr += String (UserAuth[i], HEX);
+    }
+    Serial.println("key-to-hash DocMac:");
+    Serial.println(UserAuthStr);
+    
+  userhash.reset();
+  userhash.update(UserAuth, 32);
+  userhash.finalize(UserSessionid, 32);
   String UserSessionidstr;
   for (int i = 0; i != 32; i++) {
     UserSessionidstr += String (UserSessionid[i], HEX);
